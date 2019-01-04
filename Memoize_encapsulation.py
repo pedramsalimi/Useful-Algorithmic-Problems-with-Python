e3de2325a930b937b9530e34b142dbddedef434f
pedramsalimi <salimi.733726@gmail.com>
2019-01-04 23:42:25 +0330

    Encapsulation of  memoization

---
+++ Memoize_encapsulation.py
@@ -0,0 +1,18 @@
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+def factorial(k):
+
+    if k < 2:
+        return 1
+
+    return k * factorial(k - 1)
+
+factorials = Memoize(factorial)
+print(factorials(5))
